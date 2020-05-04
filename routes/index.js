ee0de6786e9ab595e9282b1aaa98c0f195353da7
brakdag <brakdag@gmail.com>
2020-05-04 00:20:14 -0300

    configuracion

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const store = require('../lib/store')
 
 router.get('/getdata', function (req, res) {
-    store.getdata(function(x){res.send(x)})
-})
+    store.getdata(function(x){
+    	console.log(JSON.stringify(x))
+    	res.send(JSON.stringify(x))})
+});
 
+router.get('/setdata', function (req, res) {
+
+});
 
 
 router.get('/', (req, res) => { res.render('index.ejs') })
